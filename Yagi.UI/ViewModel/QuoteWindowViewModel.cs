daade27d8b81c04f863829299c2b5822b3292973
YetAnotherGreatRepo <scriptdude+yagr@gmail.com>
2015-02-19 17:21:58 +0100

    Added WPF project

---
+++ Yagi.UI/ViewModel/QuoteWindowViewModel.cs
@@ -0,0 +1,53 @@
+﻿namespace Yagi.UI.ViewModel
+{
+    using System.ComponentModel;
+    using System.Runtime.CompilerServices;
+
+    using Yagi.Core.Model;
+    using Yagi.UI.Annotations;
+
+    public class QuoteWindowViewModel : INotifyPropertyChanged
+    {
+        private string text;
+
+        private string author;
+
+        public string Text
+        {
+            get
+            {
+                return text;
+            }
+            set
+            {
+                text = value;
+                OnPropertyChanged("Text");
+            }
+        }
+
+        public string Author
+        {
+            get
+            {
+                return author;
+            }
+            set
+            {
+                author = value;
+                OnPropertyChanged("Author");
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            var handler = PropertyChanged;
+            if (handler != null)
+            {
+                handler(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+}
---
+++ Yagi.UI/Windows/QuoteWindow.xaml.cs
@@ -0,0 +1,15 @@
+﻿namespace Yagi.UI.Windows
+{
+    using System.Windows;
+
+    /// <summary>
+    /// Interaction logic for QuoteWindow.xaml
+    /// </summary>
+    public partial class QuoteWindow : Window
+    {
+        public QuoteWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
