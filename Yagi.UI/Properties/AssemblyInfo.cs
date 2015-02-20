1430dec47122f0c25c0b333c3c377d8be9015bd7
YetAnotherGreatRepo <scriptdude+yagr@gmail.com>
2015-02-20 14:46:32 +0100

    Added InstallShield

--- Yagi.UI/Properties/AssemblyInfo.cs
+++ Yagi.UI/Properties/AssemblyInfo.cs
@@ -7,11 +7,11 @@ using System.Windows;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Yagi.UI")]
+[assembly: AssemblyTitle("Yagm")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Yagi.UI")]
+[assembly: AssemblyProduct("YetAnotherGreatMotivator")]
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
